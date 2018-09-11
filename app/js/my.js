9405619a1cafa8d63b8df69d84a9d9086d65d415
padilo <padilo@Padilo>
2018-09-11 21:22:10 +0300

    ну ты понял

--- app/js/my.js
+++ app/js/my.js
@@ -27,4 +27,18 @@ $(document).ready(function () {
         }
     });
 
+     // откроет меню
+     function openNav(){
+        $('.mobilBar').toggleClass("openNav");
+        $('#nav').toggleClass('showNav');
+    }
+
+    $('.mobilBar').on('click',function(){
+        openNav();
+    });
+
+
+
+
+
 });
